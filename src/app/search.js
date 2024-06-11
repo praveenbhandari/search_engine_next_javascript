3e118e413144b1023ba8819d416202bbe0982c70
praveenbhandari <bhandaripraveen3105@gmail.com>
2024-06-11 15:00:06 +0530

    "free searches"

--- src/app/search.js
+++ src/app/search.js
@@ -1111,7 +1111,7 @@ function Search_content(//{
 
     if (searchCount > 2) {
       if(!user){
-      toast(`Please Signup for more searches`, { // Display a notification with the updated search count
+      toast(`🦄 Please Signup for more free searches`, { // Display a notification with the updated search count
         position: "top-center",
         autoClose: 15000,
         hideProgressBar: false,
